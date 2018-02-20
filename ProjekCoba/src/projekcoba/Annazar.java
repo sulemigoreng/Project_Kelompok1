458747d82efe2697a83a0c9131d360d5a65767c3
Ilham Gibran <ilgibrz@gmail.com>
2018-02-20 10:44:38 +0700

    Edit Annazar.java
    dshajdkhaskjdhaks

--- ProjekCoba/src/projekcoba/Annazar.java
+++ ProjekCoba/src/projekcoba/Annazar.java
@@ -15,10 +15,6 @@ public class Annazar {
         System.out.println("Aku Gibrann...!!!");
         System.out.println("Hai, Aku Annazar...!!!");
         System.out.println("Hai juga, Aku Hamparasut...!!!");
-<<<<<<< HEAD
-=======
         System.out.println("Hai juga, Hamparasut...!!!");
-        System.out.println("Nanda yooo, teeemmmmeeeee...!!!");
->>>>>>> f7950454eb85cb8ec48a0e8fa9d88e9fed7ba5c4
     }
 }
