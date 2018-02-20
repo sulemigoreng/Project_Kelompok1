3b8c27f863288715501e1ff45fc6af7d3b1bfa84
AfifRana <36437450+AfifRana@users.noreply.github.com>
2018-02-20 11:01:01 +0700

    Testing Afif

---
+++ ProjekCoba/src/projekcoba/Afif.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekcoba;
+
+/**
+ *
+ * @author ACER Z3-451
+ */
+public class Afif {
+    public static void main (String[] args) {
+        System.out.println ("Testing, satu.. dua.. tiga..");
+    }  
+}
