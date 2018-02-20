fca3f24ad3b70d49ee1e6dbf9ee22e5daaa5275e
sulemigoreng <sulemigoreng@gmail.com>
2018-02-20 09:13:13 +0700

    Ini coba commit

---
+++ ProjekCoba/src/projekcoba/ProjekCoba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekcoba;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProjekCoba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+    
+}
